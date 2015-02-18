c58a423f2774493ba9ef912c9ae18f0e68bad4e2
Tomohiro Nishimura <tomohiro68@gmail.com>
2015-02-18 22:19:34 +0900

    initial commit

---
+++ menu.js
@@ -0,0 +1,23 @@
+
+chrome.contextMenus.create({
+  "type"     : "normal",
+  "title"    : "Copy Repo",
+  "contexts" : ["page"],
+  "onclick"  : copyRepoName,
+  "documentUrlPatterns" : ["https://github.com/*/*"]
+});
+
+function copyRepoName(info) {
+  var url = info.pageUrl;
+  var parts = url.split("/");
+  var repo = parts.pop();
+  var userName = parts.pop();
+  var repoName = userName + "/" + repo;
+  var textArea = document.createElement("textarea");
+  textArea.style.cssText = "visibility: none;";
+  document.body.appendChild(textArea);
+  textArea.value = repoName;
+  textArea.select();
+  document.execCommand("copy");
+}
+
