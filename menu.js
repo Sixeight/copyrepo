54fd49bd43b00e39b228d33c30dbcb927466554a
Tomohiro Nishimura <tomohiro68@gmail.com>
2015-02-19 13:13:08 +0900

    add support for repository url

--- menu.js
+++ menu.js
@@ -1,21 +1,52 @@
 
-chrome.contextMenus.create({
+var parentId = chrome.contextMenus.create({
   "type"     : "normal",
   "title"    : "copyrepo",
+  "contexts" : ["page"],
+  "documentUrlPatterns" : ["https://github.com/*/*"]
+});
+
+chrome.contextMenus.create({
+  "parentId" : parentId,
+  "type"     : "normal",
+  "title"    : "Name",
   "contexts" : ["page"],
   "onclick"  : copyRepoName,
   "documentUrlPatterns" : ["https://github.com/*/*"]
 });
 
+chrome.contextMenus.create({
+  "parentId" : parentId,
+  "type"     : "normal",
+  "title"    : "URL",
+  "contexts" : ["page"],
+  "onclick"  : copyRepoURL,
+  "documentUrlPatterns" : ["https://github.com/*/*"]
+});
+
 function copyRepoName(info) {
+  var repoName = extractRepositoryName(info);
+  copyToClipBoard(repoName);
+}
+
+function copyRepoURL(info) {
+  var repoName = extractRepositoryName(info);
+  copyToClipBoard("https://github.com/" + repoName + ".git");
+}
+
+function extractRepositoryName(info) {
   var url = info.pageUrl;
   var parts = url.split("/");
   var index = parts.indexOf("github.com") + 1;
   var repoName = parts.splice(index, 2).join("/");
+  return repoName;
+}
+
+function copyToClipBoard(text) {
   var textArea = document.createElement("textarea");
   textArea.style.cssText = "visibility: none;";
   document.body.appendChild(textArea);
-  textArea.value = repoName;
+  textArea.value = text;
   textArea.select();
   document.execCommand("copy");
 }
